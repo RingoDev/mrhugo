809eab1db8b7bb69d3f0be92581f54dd49a50b53
ringodev <ringodev@protonmail.com>
2020-10-08 15:42:49 +0200

    working commit

--- static/js/main.js
+++ static/js/main.js
@@ -19,6 +19,25 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log('Endpoint URL: ', sub.endpoint);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
+}
+
 function requestNotificationPermission(){
   console.log("requested Permission");
   if ('Notification' in window && navigator.serviceWorker) {
