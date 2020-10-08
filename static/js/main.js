788cd392c22a4d6c99dbed857b6c7c5b9f668192
ringodev <ringodev@protonmail.com>
2020-10-08 11:06:47 +0200

    working commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,12 @@
 
 function requestNotificationPermission(){
-	console.log("requested Permission");
-	if ('Notification' in window && navigator.serviceWorker) {
+  console.log("requested Permission");
+  if ('Notification' in window && navigator.serviceWorker) {
   // Display the UI to let the user toggle notifications
-		console.log("Notification ability exists");
-	}
-	return;
+    console.log("Notification ability exists");
+    Notification.requestPermission(function(status) {
+      console.log('Notification permission status:', status);
+    });
+  }
+  return;
 }
