770bf17cdb434a5ec3aadaea7b4b19b6b1cb43eb
ringodev <ringodev@protonmail.com>
2020-10-08 11:34:00 +0200

    working commit

--- static/sw.js
+++ static/sw.js
@@ -326,4 +326,16 @@ self.addEventListener(
 
     }
 );
+// is called when the User clicks on the Notification
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
 
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('https://ringodev.com');
+    notification.close();
+  }
+});
