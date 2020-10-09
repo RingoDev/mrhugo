26fb4bc4fae5b61703512d88d21538b3434bf510
ringodev <ringodev@protonmail.com>
2020-10-09 22:17:09 +0200

    working commit

--- static/sw.js
+++ static/sw.js
@@ -326,7 +326,7 @@ self.addEventListener('notificationclick', function(e) {
 self.addEventListener('push', function(e) {
   if(e.data) console.log('Data of push event',e.data.text());
   var options = {
-    body: 'RingoDev was updated!',
+    body: e.data ? e.data.text() : '',
     icon: 'img/logo_1024px.png',
     data: {
       dateOfArrival: Date.now(),
